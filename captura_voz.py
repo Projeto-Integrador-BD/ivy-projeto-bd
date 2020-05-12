1df1758fe43f7392fd518bb32a00d1db560307e3
helen-alevato <helen-alevato@hotmail.com>
2020-05-12 20:31:28 -0300

    funcionalidade de captura de voz

---
+++ captura_voz.py
@@ -0,0 +1,18 @@
+# Necessário instalar o pyAudio: 
+# pip install pyaudio
+# depois instala o speech_recognition: 
+# pip install SpeechRecognition
+import speech_recognition as sr
+
+# cria uma variavel para reconhecimento do audio
+reconhecedor = sr.Recognizer()
+
+with sr.Microphone() as source:
+    # passar o que o programa ouviu para a variavel audio
+    audio = reconhecedor.listen(source)
+
+    # imprime o audio e passa para o algoritmo de reconhecimento escolhido
+    # no caso, a google
+    print(reconhecedor.recognize_google(audio, language="pt-BR"))
+
+print("FIM DO PROGRAMA")
