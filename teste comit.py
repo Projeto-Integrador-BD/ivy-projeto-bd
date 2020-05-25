be5be84b8252ecfc905700a5cba8d5a7dafc8b5a
marcelo <marceloricado.sjc@hotmail.com>
2020-05-25 20:55:08 -0300

    teste

---
+++ teste comit.py
@@ -0,0 +1,12 @@
+import time
+
+t = int(input("digite seu tempo(em segs): "))
+time.sleep(t)
+if t <=300:
+    print("vc esta sem foco, vou pegar no seu pé!")
+else:
+    print("parabens mantenha o foco!")
+
+
+
+
