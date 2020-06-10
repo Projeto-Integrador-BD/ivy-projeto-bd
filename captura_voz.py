e98e76adc4313aba2d155eeecc5861bee7ffe974
HelenAlevato <61571753+HelenAlevato@users.noreply.github.com>
2020-06-10 18:00:58 -0300

    Inserção da funcionalidade de gerar senha

--- captura_voz.py
+++ captura_voz.py
@@ -2,18 +2,52 @@
 # pip install pyaudio
 # depois instala o speech_recognition: 
 # pip install SpeechRecognition
-#test
 import speech_recognition as sr
+import pyttsx3
+from gerador_senha import gerar_senha
+
+# Inicializa o sintetizador de voz
+engine = pyttsx3.init()
 
 # cria uma variavel para reconhecimento do audio
 reconhecedor = sr.Recognizer()
 
-with sr.Microphone() as source:
-    # passar o que o programa ouviu para a variavel audio
-    audio = reconhecedor.listen(source)
+opcoes = ["senha"]
 
-    # imprime o audio e passa para o algoritmo de reconhecimento escolhido
-    # no caso, a google
-    print(reconhecedor.recognize_google(audio, language="pt-BR"))
+with sr.Microphone() as source:
+    while (True):
+        try:
+            # passar o que o programa ouviu para a variavel audio
+            audio = reconhecedor.listen(source)
+
+            # engine.say("Olá, por favor me peça uma funcionalidade")
+            # engine.runAndWait()
+
+            # imprime o audio e passa para o algoritmo de reconhecimento escolhido
+            # no caso, a google
+            frase = reconhecedor.recognize_google(audio, language="pt-BR")
+            print(frase)
+
+            if frase in opcoes:
+                if frase == "senha":
+                    engine.say("ok, estou gerando a sua senha. Aguarde um segundo")
+                    engine.runAndWait()
+                    
+                    senha_gerada = gerar_senha()
+                    
+                    engine.say("sua senha foi gerada com sucesso. Olhe na tela para verifica-la")
+                    engine.runAndWait()
+
+                break
+    
+        except Exception as err:
+            engine.say("Nao entendi, poderia falar novamente?")
+            engine.runAndWait()
+
+
+# gerar senha aleatória 
+# quando for perguntado gerar senha deve-se aparecer na tela ( Com quantos números?)
+# após a identificação do número deve-se gerar a senha com os números respectivos
+
 
 print("FIM DO PROGRAMA")
--- gerador_senha.py
+++ gerador_senha.py
@@ -1,30 +1,33 @@
 import random
 
-lista_palavras = ["batata", "morango", "abacaxi"]
-letras = ["a","b","c","d","e","f","g","i","j","k","l","m","n","o","p"]
 
-gerar_palavras = True
-gerar_numeros = True
-gerar_0_9 = True
-gerar_simbolos = False
-gerar_abc = True
-qtde_abc = 5
-gerar_maiusculo = True
+def gerar_senha():
+    lista_simbolos= ["!", "@", "#", "$", "%", "&", "*"]
+    letras = ["a", "b", "c", "d","e","f","g","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
-senha = ""
+    gerar_palavras = True
+    gerar_numeros = True
+    gerar_0_9 = True
+    gerar_simbolos = True
+    gerar_abc = True
+    qtde_abc = 5
+    gerar_maiusculo = True
 
-if gerar_palavras:
-    valor = random.randint(0, 2)
-    senha += lista_palavras[valor]
-    
-if gerar_numeros:
-    if gerar_0_9:
-        valor = random.randint(0, 9)
-        senha += str(valor)
+    senha = ""
 
-if gerar_abc:
-    for i in range(qtde_abc):
-        valor = random.randint(0, len(letras))
-        senha += letras[valor]
+    if gerar_simbolos:
+        valor = random.randint(0, len(lista_simbolos))
+        senha += lista_simbolos[valor]
+        
+    if gerar_numeros:
+        if gerar_0_9:
+            valor = random.randint(0, 9)
+            senha += str(valor)
 
-print ("sua senha é: " + senha)
+    if gerar_abc:
+        for i in range(qtde_abc):
+            valor = random.randint(0, len(letras))
+            senha += letras[valor]
+
+    print ("sua senha é: " + senha)
+    return senha
