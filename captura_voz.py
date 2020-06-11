6a7f65983cc93134763f7d7c5c8da8970ae2ebca
HelenAlevato <helen-alevato@hotmail.com>
2020-06-10 22:16:03 -0300

    consertando o arquivo de captura de voz

--- captura_voz.py
+++ captura_voz.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Necessário instalar o pyAudio: 
 # pip install pyaudio
 # depois instala o speech_recognition: 
@@ -52,25 +51,3 @@ with sr.Microphone() as source:
 
 
 print("FIM DO PROGRAMA")
-=======
-# Necessário instalar o pyAudio: 
-# pip install pyaudio
-# depois instala o speech_recognition: 
-# pip install SpeechRecognition
-#test
-import speech_recognition as sr
-
-# cria uma variavel para reconhecimento do audio
-reconhecedor = sr.Recognizer()
-
-with sr.Microphone() as source:
-    # passar o que o programa ouviu para a variavel audio
-    audio = reconhecedor.listen(source)
-
-    # imprime o audio e passa para o algoritmo de reconhecimento escolhido
-    # no caso, a google
-    print(reconhecedor.recognize_google(audio, language="pt-BR"))
-
-print("FIM DO PROGRAMA")
-print("teste")
->>>>>>> 8ce782e959178bb687571a1341415fec11508ffa
