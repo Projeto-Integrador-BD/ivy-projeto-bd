aace40099619a1443c80a116cd622e00a06276c4
rangel.andrade <jrangel.andrade@gmail.com>
2020-06-17 23:37:49 -0300

    Calculator_3

--- Ivy.py
+++ Ivy.py
@@ -5,7 +5,6 @@
 # Importar biblioteca
 import speech_recognition as sr
 import playsound
-import time
 import os
 import random
 from gtts import gTTS
@@ -19,16 +18,18 @@ def record_audio():
         audio = r.listen(source)
         voice_data = ''
         try:
-            voice_data = r.recognize_google(audio)
+            voice_data = r.recognize_google(audio, language="pt-BR")
             print(voice_data)
         except sr.UnknownValueError:
             ivy_speak('Desculpe não consegui entender')
+        except sr.RequestError:
+            ivy_speak('Desculpe, o reconhecimento de voz não está funcionando')
         return voice_data
 
 
 def ivy_speak(audio_string):
     tts = gTTS(text=audio_string, lang='pt-BR')
-    r = random.randint(1, 10000000)
+    r = random.randint(1, 1000000)
     audio_file = 'audio-' + str(r) + '.mp3'
     tts.save(audio_file)
     playsound.playsound(audio_file)
@@ -41,16 +42,22 @@ def respond(voice_data):
             ivy_speak('Ok, me diga qual operação deseja realizar? somar, subtrair, mutiplicar ou dividir?')
             voice_data = record_audio()
             if 'somar' in voice_data:
-                ivy_speak('somar, ok, então será uma operação de adição')
-            elif 'subtrair' in record_audio():
+                ivy_speak('Pode falar o primeiro numero')
+                voice_data = record_audio()
+                num1 = float(voice_data)
+                ivy_speak('Pode falar o segundo numero')
+                voice_data = record_audio()
+                num2 = float(voice_data)
+                total = num1 + num2
+                ivy_speak('O resultado é:', str(total))
+            elif 'subtrair' in voice_data:
                 ivy_speak('subtrair, ok, então será uma operação de subtração')
-            elif 'multiplicar' in record_audio():
+            elif 'multiplicar' in voice_data:
                 ivy_speak('multiplicar, ok, então será uma operação de multiplicação')
-            elif 'dividir' in record_audio():
+            elif 'dividir' in voice_data:
                 ivy_speak('dividir, ok, então será uma operação de divisão')
+        return respond(voice_data)
 
-
-#if 'ivy' in record_audio():
 ivy_speak('O que posso ajudar?')
 voice_data = record_audio()
 respond(voice_data)
